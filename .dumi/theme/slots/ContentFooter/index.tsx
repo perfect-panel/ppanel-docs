2672d1164f1aa39cf95b9e27ef9566479f8f454e
web@ppanel <web@ppanel.dev>
2024-12-27 20:27:29 +0700

    fix(ContentFooter): update Giscus language handling and remove redundant id

--- .dumi/theme/slots/ContentFooter/index.tsx
+++ .dumi/theme/slots/ContentFooter/index.tsx
@@ -13,7 +13,6 @@ export default memo(() => {
         process.env.UMI_APP_GISCUS_CATEGORY_ID && (
           <section style={{ marginTop: '32px' }}>
             <Giscus
-              id="comments"
               repo={process.env.UMI_APP_GISCUS_REPO}
               repoId={process.env.UMI_APP_GISCUS_REPO_ID}
               categoryId={process.env.UMI_APP_GISCUS_CATEGORY_ID}
@@ -23,7 +22,7 @@ export default memo(() => {
               emitMetadata="1"
               inputPosition="top"
               theme={color === 'dark' ? 'dark_protanopia' : 'light_protanopia'}
-              lang={locale}
+              lang={locale.id === 'zh-CN' ? 'zh-CN' : 'en'}
               loading="lazy"
               crossorigin="anonymous"
               async
